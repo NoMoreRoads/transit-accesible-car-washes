434068593d9f5a71d365247b8cdf61b9d2191602
Zachary Lesher <lesher.zachary@protonmail.com>
2025-11-22 19:04:00 -0800

    Analysis: finished crafting r5r query

--- analysis.R
+++ analysis.R
@@ -16,4 +16,15 @@ pop_blocks <- get_acs(
 )
 
 ## Open Street Map ====
-pdx_bb <- getbb(place_name = "Portland, Oregon", format_out = "sf_polygon")
+pdx_bb <- getbb(place_name = "Portland, Oregon", format_out = "matrix")
+op_query <- opq(bbox = pdx_bb)
+osm_query_with_features <- add_osm_features(
+  opq = op_query,
+  features = list(
+    "amenity" = "car_wash",
+    "shop" = "car_repair",
+    "amenity" = "fuel"
+  )
+)
+osm_features <- osmdata_sf(osm_query_with_features) %>%
+  unique_osmdata()
